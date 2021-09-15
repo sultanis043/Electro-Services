05a79daad3781dcce5c9317838b18bb43f441087
sultan <sultanis043@gmail.com>
2021-09-15 20:11:24 +0530

    created indivitual components and given styles to it.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,52 +4,48 @@ import MenuItem from '../menu-items/menu-item.component';
 
 import './directory.styles.scss'
 
-class Directory extends React.Component {
-    constructor(){
-        super();
-        this.state ={
-           sections: [{
-               title: 'HATS',
-               imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-               id: 1
-           },
-           {
-            title: 'JACKETS',
-            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-            id: 2
-        },
-        {
-            title: 'SNEAKERS',
-            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-            id: 3
-        },
-        {
-            title: 'WOMENS',
-            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-            size: 'large',
-            id: 4
-        },
-        {
-            title: 'MEN',
-            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-            size: 'large',
-            id: 5
-        }
-        ]
-        };
-    }
-    render(){
-
-        return(
+const Directory = () => {
+    
+    const   sections = [
+                {
+                    title: 'hats',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1
+                },
+                {
+                    title: 'jackets',
+                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    id: 2
+                },
+                {
+                    title: 'sneakers',
+                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    id: 3
+                },
+                {
+                    title: 'womens',
+                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    size: 'big',
+                    id: 4
+                },
+                {
+                    title: 'men',
+                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    size: 'big',
+                    id: 5
+                }
+            ]
+    
+        return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size})=>(
-                        <MenuItem  key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    sections.map(({ title, imageUrl, id, size }) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                     ))
                 }
+                
             </div>
         )
-    }
 }
 
 export default Directory;
